86b2331945a6d520681715df90a542d44c1c2d02
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2016-04-30 12:33:15 +0300

    [MSN-15] Profile information can be added during registration

--- client/templates/register/view.js
+++ client/templates/register/view.js
@@ -5,6 +5,17 @@ Template.register_view.onCreated(function () {
     document.title = 'MSN - Register';
 });
 
+Template.register_view.onRendered(function () {
+    const maxDate = moment().subtract(4, 'years');
+
+    $('#user-register-form-dob').datetimepicker({
+        format: 'MMMM Do YYYY',
+        useCurrent: false,
+        defaultDate: maxDate,
+        maxDate: maxDate
+    });
+});
+
 Template.register_view.events({
     /**
      * Register user in the sytem.
@@ -14,13 +25,26 @@ Template.register_view.events({
     'submit #user-register-form': function (e) {
         e.preventDefault();
 
-        /* TODO: Password repeat */
-        var email = $('#user-register-form-email').val(),
+        /* TODO: Validation */
+        let email = $('#user-register-form-email').val(),
             password = $('#user-register-form-password').val();
+
+        let inputDataArray = $('#user-register-form').find('[data-profile]').serializeArray(),
+            profileData = {};
+
+        _.each(inputDataArray, function (entity) {
+            /* Date of birth needs to be formatted */
+            if (entity.name === 'dob') {
+                entity.value = $('#user-register-form-dob').data('DateTimePicker').date().format();
+            }
+
+            profileData[entity.name] = entity.value;
+        });
 
         Accounts.createUser({
             email    : email,
-            password : password
+            password : password,
+            profile  : profileData
         }, function (error) {
             if (!error) {
                 FlowRouter.go('home');
